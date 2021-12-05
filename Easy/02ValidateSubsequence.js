af10d01418053603b9de28462d74cceabf9f62b1
Younis Rahman <younisrahman@Younis-MacBook-Pro-17.local>
2021-12-06 00:01:55 +0600

    first: sub sequences

---
+++ Easy/02ValidateSubsequence.js
@@ -0,0 +1,17 @@
+let arr = [5, 1, 22, 25, 6, -1, 8, 10];
+let subArr = [1, 22, -1, 10];
+let newSubArr = [];
+
+function checkSubsequence(arr, subArr) {
+  for (let i = 0; i < subArr.length - 1; i++) {
+    if (arr.indexOf(subArr[i]) > arr.indexOf(subArr[i + 1])) {
+      return false;
+    }
+
+    // console.log(arr.indexOf(subArr[i]));
+    // console.log(arr.includes(subArr[i + 1]));
+  }
+  return true;
+}
+
+console.log(checkSubsequence(arr, subArr));
