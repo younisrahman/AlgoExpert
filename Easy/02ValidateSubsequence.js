44dac01152ec931bb835aa8b1ef381ae142b2733
Younis Rahman <younisrahman@Younis-MacBook-Pro-17.local>
2021-12-06 11:41:43 +0600

    2nd final

--- Easy/02ValidateSubsequence.js
+++ Easy/02ValidateSubsequence.js
@@ -3,10 +3,17 @@ let subArr = [1, 22, -1, 10];
 
 function checkSubsequence(arr, subArr) {
   for (let i = 0; i < subArr.length - 1; i++) {
-    if (arr.indexOf(subArr[i]) > arr.indexOf(subArr[i + 1])) {
+    // if (arr.includes(subArr[i])) {
+    if (
+      !arr.includes(subArr[i]) ||
+      arr.indexOf(subArr[i]) > arr.indexOf(subArr[i + 1])
+    ) {
       return false;
     }
-
+    // }
+    // else {
+    //   return false;
+    // }
     // console.log(arr.indexOf(subArr[i]));
     // console.log(arr.includes(subArr[i + 1]));
   }
