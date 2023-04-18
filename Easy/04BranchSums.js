6c75bd6dac962dbd93489ce17026fd53ad3e0b2c
Younis Rahman <younisrahman007@gmail.com>
2023-04-19 00:04:28 +0600

    branch sum

--- Easy/04BranchSums.js
+++ Easy/04BranchSums.js
@@ -1,3 +1,5 @@
+// This is the class of the input root.
+// Do not edit it.
 class BinaryTree {
   constructor(value) {
     this.value = value;
@@ -6,22 +8,27 @@ class BinaryTree {
   }
 }
 
-const branchSums = (root) => {
-  let sums = [];
-  calculatBranchSum(root, 0, sums);
-  return sums;
-};
-const calculateBranchSum = (node, runingSum, sums) => {
-  if (!node) return;
-
+function calculateBranchSum (node, runingSum, sums) {
+  
   let newRunningsum = runingSum + node.value;
-  if (node.left && node.right) {
+  if (!node.left && !node.right) {
     sums.push(newRunningsum);
     return;
   }
 
+  if(node.left){
   calculateBranchSum(node.left, newRunningsum, sums);
-  calculateBranchSum(node.right, newRunningsum, sums);
+  }
+   if(node.right){
+  calculateBranchSum(node.right, newRunningsum, sums);}
 };
+
+function branchSums(root) {
+  const sums = [];
+  calculateBranchSum(root, 0, sums);
+  return sums;
+}
+
+// Do not edit the lines below.
 exports.BinaryTree = BinaryTree;
 exports.branchSums = branchSums;
