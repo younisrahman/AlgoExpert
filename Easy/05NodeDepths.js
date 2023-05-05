cbe6d9a0c2798da7e4b621bb6fa82cc7946bde92
Younis Rahman <younisrahman007@gmail.com>
2023-05-05 23:40:58 +0600

    complete: Node Depths Javascript

--- Easy/05NodeDepths.js
+++ Easy/05NodeDepths.js
@@ -1,25 +1,15 @@
-const Nodedepth = ({ root, depth = 0 }) => {
-  if (root == null) {
-    return 0;
-  }
+function nodeDepths(root) {
+  return sumDepths(root, 0);
+}
+
+function sumDepths(node, level) {
+  if (!node) return 0;
+  // return the current level plus all child levels
   return (
-    depth + Nodedepth(root.left, depth + 1) + Nodedepth(root.right, depth + 1)
+    level + sumDepths(node.left, level + 1) + sumDepths(node.right, level + 1)
   );
-};
-
-const Nodedepth2 = ({ root }) => {
-  let stack = [{ node: root, depth: 0 }];
-  let sumOfDepth = 0;
-  while (stack.length > 0) {
-    const { node, depth } = stack.pop();
-    if (node === null) continue;
-    sumOfDepth += depth;
-    stack.push({ node: node.left, depth: depth + 1 });
-    stack.push({ node: node.right, depth: depth + 1 });
-  }
-  return sumOfDepth;
-};
-
+}
+// This is the class of the input binary tree.
 class BinaryTree {
   constructor(value) {
     this.value = value;
@@ -27,3 +17,6 @@ class BinaryTree {
     this.right = null;
   }
 }
+
+// Do not edit the line below.
+exports.nodeDepths = nodeDepths;
