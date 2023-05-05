db74dd361927909139a20b627a384d586d2f6d51
Younis Rahman <younisrahman007@gmail.com>
2023-05-05 23:40:12 +0600

    complete: Node Depths Python

--- Easy/05NodeDepths.py
+++ Easy/05NodeDepths.py
@@ -1,23 +1,11 @@
-# recursive function
-# O(n) time | O(h) space
-def nodedepth(root, depth=0):
-    if root is None:
-        return 0
-    return depth + nodedepth(root.left, depth + 1) + nodedepth(root.right, depth+1)
-
-# iterative function
-# O(n) time | O(h) space
+def nodeDepths(root, depth = 0):
+    # Write your code here.
+    return 0 if root is None else depth + nodeDepths(root.left, depth+1) + nodeDepths(root.right, depth+1)    
 
 
-def findtheddepth(root):
-    stack = [{"node": root, "depth": 0}]
-    sumOfDepth = 0
-    while len(stack) > 0:
-        nodeInfo = stack.pop()
-        node, depth = nodeInfo["node"], nodeInfo["depth"]
-        if node in None:
-            continue
-        sumOfDepth += depth
-        stack.append({"node": node.left, "depth": depth+1})
-        stack.append({"node": node.right, "depth": depth+1})
-    return sumOfDepth
+# This is the class of the input binary tree.
+class BinaryTree:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
