3d5466f6ff5a22088f41ad1f3f2553ef33c1ded6
Younis Rahman <younisrahman007@gmail.com>
2023-05-31 02:12:08 +0600

    09EevaluateExpressionTree

---
+++ 09EevaluateExpressionTree.py
@@ -0,0 +1,11 @@
+# This is an input class. Do not edit.
+class BinaryTree:
+    def __init__(self, value, left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
+
+
+def evaluateExpressionTree(tree):
+    # Write your code here.
+    return -1
