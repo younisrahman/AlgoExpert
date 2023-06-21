45aa2425dfa0ee3804e74a604e078b829bcb64b0
Younis Rahman <younisrahman007@gmail.com>
2023-06-22 02:29:33 +0600

    35

---
+++ Medium/35SpiralTraverse.py
@@ -0,0 +1,37 @@
+def spiralTraverse(array):
+    result = []
+    startRow, endRow = 0, len(array)-1
+    startCol, endCol = 0, len(array[0])-1
+
+    while startRow <= endRow and startCol <= endCol:
+
+        for col in range(startCol, endCol+1):
+            print(array[startRow][col])
+            result.append(array[startRow][col])
+
+        for row in range(startRow+1, endRow+1):
+            result.append(array[row][endCol])
+
+        for col in reversed(range(startCol, endCol)):
+            if startRow == endRow:
+                break
+            result.append(array[endRow][col])
+
+        for row in reversed(range(startRow+1, endRow)):
+            if startCol == endCol:
+                break
+            result.append(array[row][startCol])
+
+        startRow += 1
+        endRow -= 1
+        startCol += 1
+        endCol -= 1
+
+    return result
+
+
+print(spiralTraverse([
+    [1, 2, 3, 4],
+    [10, 11, 12, 5],
+    [9, 8, 7, 6]
+]))
