4d78650f4a5c064b1fdcb84c2e5612c3d7c78375
Younis Rahman <younisrahman007@gmail.com>
2023-06-22 02:43:54 +0600

    35

--- Medium/35SpiralTraverse.js
+++ Medium/35SpiralTraverse.js
@@ -1,3 +1,41 @@
+function spiralTraverse(array) {
+  const result = [];
+  let startRow = 0;
+  let endRow = array.length - 1;
+  let startCol = 0;
+  let endCol = array[0].length - 1;
+
+  while (startRow <= endRow && startCol <= endCol) {
+    for (let col = startCol; col <= endCol; col++) {
+      result.push(array[startRow][col]);
+    }
+
+    for (let row = startRow + 1; row <= endRow; row++) {
+      result.push(array[row][endCol]);
+    }
+
+    for (let col = endCol - 1; col >= startCol; col--) {
+      if (startRow === endRow) {
+        break;
+      }
+      result.push(array[endRow][col]);
+    }
+
+    for (let row = endRow - 1; row >= startRow + 1; row--) {
+      if (startCol === endCol) {
+        break;
+      }
+      result.push(array[row][startCol]);
+    }
+
+    startRow++;
+    endRow--;
+    startCol++;
+    endCol--;
+  }
+
+  return result;
+}
 console.log("====================================");
 console.log(
   spiralTraverse([
