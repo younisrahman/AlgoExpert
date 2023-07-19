c87279ed41cdcf0b2cebcc9c5f37f99aa7961f66
Younis Rahman <younisrahman007@gmail.com>
2023-07-20 01:35:58 +0600

    minHeightBst

---
+++ Medium/48MinimumBST.py
@@ -0,0 +1,21 @@
+def minHeightBst(array):
+    pass
+
+
+class BST:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    def insert(self, value):
+        if value < self.value:
+            if self.left is None:
+                self.left = BST(value)
+            else:
+                self.left.insert(value)
+        else:
+            if self.right is None:
+                self.right = BST(value)
+            else:
+                self.right.insert(value)
