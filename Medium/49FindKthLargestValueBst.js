d59cd13509af73f969c1180ea8dd998b1c9e18d4
Younis Rahman <younisrahman007@gmail.com>
2023-07-22 02:14:54 +0600

    findKthLargestValueInBst

--- Medium/49FindKthLargestValueBst.js
+++ Medium/49FindKthLargestValueBst.js
@@ -1,16 +1,52 @@
+//O(n) space(n)
+// function findKthLargestValueInBst(tree, k) {
+//   // Write your code here.
+//   let sortedNodeValue = [];
+//   inOrderTraversal(tree, sortedNodeValue);
+//   return sortedNodeValue[sortedNodeValue.length - k];
+// }
+
+// function inOrderTraversal(tree, sortedNodeValue) {
+//   if (tree === null) {
+//     return;
+//   }
+
+//   inOrderTraversal(tree.left, sortedNodeValue);
+//   sortedNodeValue.push(tree.value);
+//   inOrderTraversal(tree.right, sortedNodeValue);
+// }
+
+//O(k+h) space(k+h)
+// function findKthLargestValueInBst(tree, k) {
+//   // Write your code here.
+//   let sortedNodeValue = [];
+//   inOrderTraversal(tree, sortedNodeValue, k);
+//   return sortedNodeValue[k - 1];
+// }
+
+// function inOrderTraversal(tree, sortedNodeValue, k) {
+//   if (tree === null || sortedNodeValue.length === k) {
+//     return;
+//   }
+
+//   inOrderTraversal(tree.right, sortedNodeValue, k);
+//   sortedNodeValue.push(tree.value);
+//   inOrderTraversal(tree.left, sortedNodeValue, k);
+// }
+
 function findKthLargestValueInBst(tree, k) {
   // Write your code here.
   let sortedNodeValue = [];
-  inOrderTraversal(tree, sortedNodeValue);
-  return sortedNodeValue[sortedNodeValue.length - k];
+  inOrderTraversal(tree, sortedNodeValue, k);
+  return sortedNodeValue[k - 1];
 }
 
-function inOrderTraversal(tree, sortedNodeValue) {
-  if (tree === null) {
+function inOrderTraversal(tree, sortedNodeValue, k) {
+  if (tree === null || sortedNodeValue.length === k) {
     return;
   }
 
-  inOrderTraversal(tree.left, sortedNodeValue);
+  inOrderTraversal(tree.right, sortedNodeValue, k);
   sortedNodeValue.push(tree.value);
-  inOrderTraversal(tree.right, sortedNodeValue);
+  inOrderTraversal(tree.left, sortedNodeValue, k);
 }
