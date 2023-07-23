0dec13d8aa32fe9ca81b5f7c56bea3e0da0f8b03
Younis Rahman <younisrahman007@gmail.com>
2023-07-23 22:05:04 +0600

    BST

---
+++ Medium/50reconstructBst.py
@@ -0,0 +1,28 @@
+# This is an input class. Do not edit.
+class BST:
+    def __init__(self, value, left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
+
+
+def reconstructBst(preOrderTraversalValues):
+    # Write your code here.
+    if len(preOrderTraversalValues) == 0:
+        return None
+
+    currentValue = preOrderTraversalValues[0]
+    rightSubtreeRootIdx = len(preOrderTraversalValues)
+
+    for idx in range(1, len(preOrderTraversalValues)):
+        value = preOrderTraversalValues[idx]
+        if value >= currentValue:
+            rightSubtreeRootIdx = idx
+            break
+
+    leftSubtree = reconstructBst(
+        preOrderTraversalValues[1:rightSubtreeRootIdx])
+    rightSubtree = reconstructBst(
+        preOrderTraversalValues[rightSubtreeRootIdx:])
+
+    return BST(currentValue, leftSubtree, rightSubtree)
