f28406ef03f1d18b994a4d86b292b93f37874b87
Younis Rahman <younisrahman007@gmail.com>
2023-07-23 22:27:48 +0600

    50 reconstructBst

--- Medium/50ReconstructBst.js
+++ Medium/50ReconstructBst.js
@@ -1,38 +1,86 @@
-// This is an input class. Do not edit.
-class BST {
-  constructor(value, left = null, right = null) {
-    this.value = value;
-    this.left = left;
-    this.right = right;
+// // This is an input class. Do not edit.
+// class BST {
+//   constructor(value, left = null, right = null) {
+//     this.value = value;
+//     this.left = left;
+//     this.right = right;
+//   }
+// }
+
+// function reconstructBst(preOrderTraversalValues) {
+//   if (preOrderTraversalValues.length === 0) {
+//     return null;
+//   }
+
+//   let currentValue = preOrderTraversalValues[0];
+//   let rightSubtreeRootIdx = preOrderTraversalValues.length;
+
+//   for (let idx = 1; idx < preOrderTraversalValues.length; idx++) {
+//     const value = preOrderTraversalValues[idx];
+//     if (value >= currentValue) {
+//       rightSubtreeRootIdx = idx;
+//       break;
+//     }
+//   }
+
+//   let leftSubtree = reconstructBst(
+//     preOrderTraversalValues.slice(1, rightSubtreeRootIdx)
+//   );
+//   let rightSubtree = reconstructBst(
+//     preOrderTraversalValues.slice(rightSubtreeRootIdx)
+//   );
+
+//   return new BST(currentValue, leftSubtree, rightSubtree);
+// }
+
+// // Do not edit the lines below.
+// exports.BST = BST;
+// exports.reconstructBst = reconstructBst;
+
+class TreeInfo {
+  constructor(rootIdx) {
+    this.rootIdx = rootIdx;
   }
 }
 
 function reconstructBst(preOrderTraversalValues) {
-  if (preOrderTraversalValues.length === 0) {
+  let treeInfo = new TreeInfo(0);
+
+  return reconstructBstFromRange(
+    -Infinity,
+    Infinity,
+    preOrderTraversalValues,
+    treeInfo
+  );
+}
+
+function reconstructBstFromRange(
+  lowerBound,
+  upperBound,
+  preOrderTraversalValues,
+  currentSubtreeInfo
+) {
+  if (currentSubtreeInfo.rootIdx === preOrderTraversalValues.length) {
+    return null;
+  }
+  let rootValue = preOrderTraversalValues[currentSubtreeInfo.rootIdx];
+  if (rootValue < lowerBound || rootValue >= upperBound) {
     return null;
   }
 
-  let currentValue = preOrderTraversalValues[0];
-  let rightSubtreeRootIdx = preOrderTraversalValues.length;
-
-  for (let idx = 1; idx < preOrderTraversalValues.length; idx++) {
-    const value = preOrderTraversalValues[idx];
-    if (value >= currentValue) {
-      rightSubtreeRootIdx = idx;
-      break;
-    }
-  }
-
-  let leftSubtree = reconstructBst(
-    preOrderTraversalValues.slice(1, rightSubtreeRootIdx)
+  currentSubtreeInfo.rootIdx++;
+  let leftSubTree = reconstructBstFromRange(
+    lowerBound,
+    rootValue,
+    preOrderTraversalValues,
+    currentSubtreeInfo
   );
-  let rightSubtree = reconstructBst(
-    preOrderTraversalValues.slice(rightSubtreeRootIdx)
+  let rightSubTree = reconstructBstFromRange(
+    rootValue,
+    upperBound,
+    preOrderTraversalValues,
+    currentSubtreeInfo
   );
 
-  return new BST(currentValue, leftSubtree, rightSubtree);
+  return new BST(rootValue, leftSubTree, rightSubTree);
 }
-
-// Do not edit the lines below.
-exports.BST = BST;
-exports.reconstructBst = reconstructBst;
--- Medium/50reconstructBst.py
+++ Medium/50reconstructBst.py
@@ -6,23 +6,63 @@ class BST:
         self.right = right
 
 
+# def reconstructBst(preOrderTraversalValues):
+#     # Write your code here.
+#     if len(preOrderTraversalValues) == 0:
+#         return None
+
+#     currentValue = preOrderTraversalValues[0]
+#     rightSubtreeRootIdx = len(preOrderTraversalValues)
+
+#     for idx in range(1, len(preOrderTraversalValues)):
+#         value = preOrderTraversalValues[idx]
+#         if value >= currentValue:
+#             rightSubtreeRootIdx = idx
+#             break
+
+#     leftSubtree = reconstructBst(
+#         preOrderTraversalValues[1:rightSubtreeRootIdx])
+#     rightSubtree = reconstructBst(
+#         preOrderTraversalValues[rightSubtreeRootIdx:])
+
+#     return BST(currentValue, leftSubtree, rightSubtree)
+
+
+class TreeInfo:
+    def __init__(self, rootIdx):
+        self.rootIdx = rootIdx
+
+
 def reconstructBst(preOrderTraversalValues):
-    # Write your code here.
-    if len(preOrderTraversalValues) == 0:
+    treeInfo = TreeInfo(0)
+    return reconstructBstFromRange(
+        float('-inf'),
+        float('inf'),
+        preOrderTraversalValues,
+        treeInfo
+    )
+
+
+def reconstructBstFromRange(lowerBound, upperBound, preOrderTraversalValues, currentSubtreeInfo):
+    if currentSubtreeInfo.rootIdx == len(preOrderTraversalValues):
         return None
 
-    currentValue = preOrderTraversalValues[0]
-    rightSubtreeRootIdx = len(preOrderTraversalValues)
+    rootValue = preOrderTraversalValues[currentSubtreeInfo.rootIdx]
+    if rootValue < lowerBound or rootValue >= upperBound:
+        return None
 
-    for idx in range(1, len(preOrderTraversalValues)):
-        value = preOrderTraversalValues[idx]
-        if value >= currentValue:
-            rightSubtreeRootIdx = idx
-            break
+    currentSubtreeInfo.rootIdx += 1
+    leftSubTree = reconstructBstFromRange(
+        lowerBound,
+        rootValue,
+        preOrderTraversalValues,
+        currentSubtreeInfo
+    )
+    rightSubTree = reconstructBstFromRange(
+        rootValue,
+        upperBound,
+        preOrderTraversalValues,
+        currentSubtreeInfo
+    )
 
-    leftSubtree = reconstructBst(
-        preOrderTraversalValues[1:rightSubtreeRootIdx])
-    rightSubtree = reconstructBst(
-        preOrderTraversalValues[rightSubtreeRootIdx:])
-
-    return BST(currentValue, leftSubtree, rightSubtree)
+    return BST(rootValue, leftSubTree, rightSubTree)
