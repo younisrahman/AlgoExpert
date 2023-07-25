e843b4dc3886c9d458cda91aec5d2c9e29908d67
Younis Rahman <younisrahman007@gmail.com>
2023-07-26 02:34:09 +0600

    invertBinaryTree js

---
+++ Medium/51InvertBinaryTree.js
@@ -0,0 +1,30 @@
+// O(n) time | O(n) space
+// function invertBinaryTree(tree) {
+//   // Write your code here.
+//   let queue = [tree];
+//   while (queue.length > 0) {
+//     let current = queue.shift();
+//     if (current === null) continue;
+
+//     swapLeftAndRight(current);
+//     queue.push(current.left);
+//     queue.push(current.right);
+//   }
+// }
+
+// O(n) time | O(d) space
+function invertBinaryTree(tree) {}
+function swapLeftAndRight(tree) {
+  let temp = tree.left;
+  tree.left = tree.right;
+  tree.right = temp;
+}
+
+// This is the class of the input binary tree.
+class BinaryTree {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
