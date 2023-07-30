21d71dfcccffd33ece2f82f42432220b9ea40c57
Younis Rahman <younisrahman007@gmail.com>
2023-07-30 17:07:23 +0600

    findSuccessor

--- Medium/53FindSuccessor.py
+++ Medium/53FindSuccessor.py
@@ -7,25 +7,48 @@ class BinaryTree:
         self.parent = parent
 
 
+# def findSuccessor(tree, node):
+#     inOrderTraversalOrder = getInOrderTraversalOrder(tree)
+
+#     for idx, currentNode in enumerate(inOrderTraversalOrder):
+#         if currentNode != node:
+#             continue
+
+#         if idx == len(inOrderTraversalOrder) - 1:
+#             return None
+#         return inOrderTraversalOrder[idx+1]
+
+
+# def getInOrderTraversalOrder(node, orders=[]):
+
+#     if node is None:
+#         return orders
+
+#     getInOrderTraversalOrder(node.left, orders)
+#     orders.append(node)
+#     getInOrderTraversalOrder(node.right, orders)
+
+#     return orders
+
+
 def findSuccessor(tree, node):
-    inOrderTraversalOrder = getInOrderTraversalOrder(tree)
-
-    for idx, currentNode in enumerate(inOrderTraversalOrder):
-        if currentNode != node:
-            continue
-
-        if idx == len(inOrderTraversalOrder) - 1:
-            return None
-        return inOrderTraversalOrder[idx+1]
+    if node.right is not None:
+        return getLeftMostChild(node)
+    return getRightMostChild(node)
 
 
-def getInOrderTraversalOrder(node, orders=[]):
+def getLeftMostChild(node):
+    currentNode = node.right
 
-    if node is None:
-        return orders
+    while currentNode.left is not None:
+        currentNode = currentNode.left
+    return currentNode
 
-    getInOrderTraversalOrder(node.left, orders)
-    orders.append(node)
-    getInOrderTraversalOrder(node.right, orders)
 
-    return orders
+def getRightMostChild(node):
+    currentNode = node
+
+    while currentNode.parent is not None and currentNode.parent.right == currentNode:
+        currentNode = currentNode.parent
+
+    return currentNode.parent
