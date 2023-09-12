49aab87799cf6cc102c1821652e4e56070fb6514
Younis Rahman <younisrahman007@gmail.com>
2023-09-13 01:44:32 +0600

    numberOfWaysToMakeChange

---
+++ Medium/59NumberOfWaysToMakeChange.js
@@ -0,0 +1,13 @@
+function numberOfWaysToMakeChange(n, denoms) {
+  let ways = new Array(n + 1).fill(0);
+  ways[0] = 1;
+  for (let denom of denoms) {
+    // Corrected 'demon' to 'denom'
+    for (let amount = 1; amount < n + 1; amount++) {
+      if (denom <= amount) {
+        ways[amount] += ways[amount - denom];
+      }
+    }
+  }
+  return ways[n];
+}
